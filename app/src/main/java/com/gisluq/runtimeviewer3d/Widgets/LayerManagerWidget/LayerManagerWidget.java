0873ec6398ce4b65e95b36bcdf4ac9fa6f831b3d
luq <luqiang.gis@foxmail.com>
2018-06-13 22:20:23 +0800

    解决geopackage无数时，widget异常

--- app/src/main/java/com/gisluq/runtimeviewer3d/Widgets/LayerManagerWidget/LayerManagerWidget.java
+++ app/src/main/java/com/gisluq/runtimeviewer3d/Widgets/LayerManagerWidget/LayerManagerWidget.java
@@ -252,6 +252,7 @@ public class LayerManagerWidget extends BaseWidget {
     private void initGeoPackageLayers(){
         String path = getGeoPackagePath();
         List<FileUtils.FileInfo> fileInfos = FileUtils.getFileListInfo(path,"gpkg");
+        if (fileInfos==null) return;
         for (int i = 0; i < fileInfos.size(); i++) {
             FileUtils.FileInfo fileInfo = fileInfos.get(i);
             final GeoPackage geoPackage = new GeoPackage(fileInfo.FilePath);
